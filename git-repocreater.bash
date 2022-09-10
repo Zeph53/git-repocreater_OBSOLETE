7099a86d7f2d3dc7626c1fa98f748d6205236e0f
root <anonymoose.snowman@gmail.com>
2022-09-10 19:26:44 -0400

    commit

--- git-repocreater.bash
+++ git-repocreater.bash
@@ -58,7 +58,7 @@ if [[ $1 ]]; then
     git branch -M main ; 
     GITUSERTEMP="$(mktemp)"
     gh auth status &> $GITUSERTEMP ;
-    nano $GITUSERTEMP | grep "Logged" ; 
+    cat >> $GITUSERTEMP | grep "Logged" ; 
     git remote add origin https://github.com/$GITUSERNAME/$REPONAME.git ; 
     DESCRIPTION="$(mktemp)" 
     printf "\nCreate a description: Control-D to continue\n" ; 
